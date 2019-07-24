7cce821e1e66ee60eec30e2e982323a93861dda4
nilay <bhatianilay@gmail.com>
2019-07-24 23:10:13 +0530

    Completed question_detail page

--- forum/urls.py
+++ forum/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.question_list, name='question_list'),
+	path('question/<int:pk>', views.question_detail, name = 'question_detail'), 
 ]
--- forum/views.py
+++ forum/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import User, TeamMember, Question, Answer
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 
 def question_list(request):
-		questions = Question.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+		questions = Question.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
 		return render(request, 'forum/question_list.html', {'questions': questions})
 
+def question_detail(request, pk):
+		question = get_object_or_404(Question, pk = pk)
+		answers = Answer.objects.filter (answer_to = question)
+		answer_count = len(answers)
+		return render(request, 'forum/question_detail.html', { 'question' : question, 'answers' : answers, 'answer_count' : answer_count})
 
